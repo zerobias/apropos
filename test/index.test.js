a94aa036404b4a7ed09830a88fc39e83fadff5f8
Dmitry Boldyrev <ribkatt@gmail.com>
2017-08-11 09:51:29 +0300

    Add jest tests
    Signed-off-by: Dmitry Boldyrev <ribkatt@gmail.com>

---
+++ test/index.test.js
@@ -0,0 +1,60 @@
+const { is, of, Left, Right, makeError } = require('../lib')
+
+
+
+describe('smoke test', () => {
+  test('Right value', () => {
+    const data = {}
+    const r = of(data)
+    const r1 = Right(data)
+
+    expect(r).toHaveProperty('value', data)
+    expect(r.isRight()).toBe(true)
+    expect(r.isLeft()).toBe(false)
+
+    expect(r1).toHaveProperty('value', data)
+    expect(r1.isRight()).toBe(true)
+    expect(r1.isLeft()).toBe(false)
+  })
+
+  test('Left value', () => {
+    const data = {}
+    const l = Left(data)
+    expect(l).toHaveProperty('value', data)
+    expect(l.isRight()).toBe(false)
+    expect(l.isLeft()).toBe(true)
+  })
+
+
+  test('toString', () => {
+    const data = 10
+    const r = of(data)
+    const l = Left(data)
+    expect(r.toString()).toBe(`Right( 10 )`)
+    expect(l.toString()).toBe(`Left( 10 )`)
+  })
+
+})
+
+
+test('is', () => {
+  const data = {}
+  const r = of(data)
+  const l = Left(data)
+  expect(is(r)).toBe(true)
+  expect(is(l)).toBe(true)
+  expect(is(data)).toBe(false)
+})
+
+
+test('annotated error', () => {
+  const data = {}
+  const createTaggedError = makeError('error with tag')
+  const errorInstance = createTaggedError(data)
+  expect(errorInstance).toMatchObject({
+    name: 'AnnotatedError',
+    tag : 'error with tag',
+    data
+  })
+})
+
